25a78bc62485d8732e87f738f6a8be4b011cda29
Martin Araya <martin.araya.zavala@gmail.com>
2025-09-09 15:24:46 -0300

    FEAT: Add initial Django project files and configurations

--- back/back/settings.py
+++ back/back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q72tbeq!c1l5=a^^-v(pjm6gfup4ima650!z=pj8^_2+@4jyaz'
+SECRET_KEY = 'django-insecure-6lq1zzislo96k6t16k8zgg9b$kk=k21l#_2uprszljsymt@hn6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
