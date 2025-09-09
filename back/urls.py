d12a9a11cf570c82dec9c838849e116d81d159d1
Martin Araya <martin.araya.zavala@gmail.com>
2025-09-09 15:59:57 -0300

    FEAT:: Updated backend to use routers

--- back/urls.py
+++ back/urls.py
@@ -15,16 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from tasks.views import TaskViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tasks import views
 
-task_list = TaskViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tasks/', task_list, name='snippet-list')
-
-
+router = DefaultRouter()
+router.register(r'tasks', views.TaskViewSet, basename='task')
+urlpatterns = [ 
+    path('', include(router.urls)),
 ]
